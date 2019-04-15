19467d785d7706b7d7903406075831b65a6a76ff
sadiev <sadiev@itstep.kz>
2019-04-15 21:07:43 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Automobile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+  class Automobile
+  {
+    public string Name { get; set; }
+    public void StartRide()
+    {
+      Start(Name, Speed(new Random(), 500, 900));
+    }
+  }
+}
---
+++ ConsoleApp8/SportCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+  class SportCar : Car
+  {
+    public string Name { get; set; }
+    public void StartRide()
+    {
+      Start(Name, Speed(new Random(), 600, 1000));
+    }
+  }
+}
